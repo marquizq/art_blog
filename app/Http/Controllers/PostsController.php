845971bebc1a89b030c1bd11cc4503e4e280f19a
Michael Mallya <michaelmallya@gmail.com>
2020-11-05 17:41:30 +0300

    Added some styles and fonts also updated a controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,29 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Facades\DB;
+
 class PostsController extends Controller
 {
+
     public function show($slug)
     {
+
         return view('post',[
-            'post' => Post::where('slug',$slug)->firstOrFail()
+           'post' => Post::where('slug',$slug)->firstOrFail()
         ]);
 
-    }
-    public function index($post)
-    {
-        $posts = [
-          'my-first-post' => 'This is my first post',
-          'my-second-post' => 'This is on going posts',
-        ];
-
-//        if(!array_key_exists($post,$posts)){
-//            abort(404,'Sorry the page is not found');
-//        }
-//
-//
-//        return view('post',[
-//            'post' => $posts[$post]
-//        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/posts/{post}','PostsController@show');
+
+
+
+
+
 Route::get('/',function (){
     return view('welcome');
+});
+
+Route::get('/about',function (){
+    return view('about');
+});
+Route::get('/contact',function (){
+    return view('contact');
 });
